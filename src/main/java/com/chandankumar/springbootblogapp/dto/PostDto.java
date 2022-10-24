c731e941d6956f729277b55ac2c10e8290505f4f
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2022-10-24 18:24:48 +0530

    Added Spring boot validation Lib

--- src/main/java/com/chandankumar/springbootblogapp/dto/PostDto.java
+++ src/main/java/com/chandankumar/springbootblogapp/dto/PostDto.java
@@ -3,15 +3,28 @@ package com.chandankumar.springbootblogapp.dto;
 import com.chandankumar.springbootblogapp.model.Comment;
 import lombok.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class PostDto {
 
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 3 letters")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 3 letters")
     private String description;
+
+    @NotEmpty
+    @Size(min = 15, message = "Post content should have at least 3 letters")
     private  String content;
+
     private Set<CommentDto> comments;
 
 }

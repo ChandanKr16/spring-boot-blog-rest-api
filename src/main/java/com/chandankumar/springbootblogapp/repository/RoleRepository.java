6fc36a6b6059713442718c4ecc256b390f7aac60
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-02-21 12:40:43 +0530

    Create UserRepository and RoleRepository

---
+++ src/main/java/com/chandankumar/springbootblogapp/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.chandankumar.springbootblogapp.repository;
+
+import com.chandankumar.springbootblogapp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+}
